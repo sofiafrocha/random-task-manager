d2c6e23874416c4e42926264be42aefa8b8a1dbc
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2020-01-04 16:33:50 +0000

    Add initial logic

--- assets/components/CobaiaApp.vue
+++ assets/components/CobaiaApp.vue
@@ -1,24 +1,132 @@
 <template>
-    <div class="CobaiaApp">
-        {{ message }}
+    <div class="a-RandomTask">
+        <h1>Random Task Manager</h1>
+        <input type="text" name="new_task" id="">
+        <button>Add task</button>
+        <ol>
+            <li v-for="task in tasks" :class="{ 'is-done': task.status === 'done'}">
+                <span>
+                    {{ task.text }}
+                </span>
+                <span v-for="emoji in task.pomodoros">🍅</span>
+            </li>
+        </ol>
+
+        <hr>
+
+        <button @click="showNextTask">Get next task</button>
+
+        <div v-show="selectedTaskIndex != null" class="a-RandomTask__next">
+            <h2>Your next task:</h2>
+            <cite v-if="selectedTaskIndex != null">
+                {{ tasks[selectedTaskIndex].text }}
+            </cite>
+            <br>
+            <br>
+            <button @click="addPomodoro">I did a 🍅</button>
+
+            <div v-show="showNextTaskFollowup">
+                <button @click="finishTask">Finished the task</button>
+                <button>Didn't finish the task</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'CobaiaApp',
-        data() {
-            return {
-                message: 'this message is coming from a Vue component!',
+const LINES = 32;
+
+export default {
+    name: 'a-RandomTask',
+    data() {
+        return {
+            message: 'this message is coming from a Vue component!',
+            hasNextTask: true,
+            showNextTaskFollowup: false,
+            tasks: [
+                {
+                    text: 'Go shopping',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+                {
+                    text: 'Sharpen knives',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+                {
+                    text: 'Prepare ingredients',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+                {
+                    text: 'Cook food',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+                {
+                    text: 'Set table',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+                {
+                    text: 'Eat delicious food',
+                    pomodoros: 0,
+                    status: 'to-do',
+                },
+            ],
+            selectedTaskIndex: null,
+        }
+    },
+    methods: {
+        showNextTask() {
+            this.selectedTaskIndex = this.getNextTaskIndex();
+
+            console.log('selectedTaskIndex', this.selectedTaskIndex);
+        },
+        getNextTaskIndex() {
+            let randomInt = Math.random() * LINES;
+            randomInt = Math.round(randomInt);
+
+            console.log('random number!', randomInt);
+
+            if (randomInt < this.tasks.length) {
+                return randomInt;
+            }  else {
+                const resto = randomInt % this.tasks.length;
+                console.log('resto', resto);
+
+                return resto;
             }
+        },
+        addPomodoro() {
+            this.tasks[this.selectedTaskIndex].pomodoros += 1;
+            this.showNextTaskFollowup = true;
+        },
+        finishTask() {
+            this.tasks[this.selectedTaskIndex].status = 'done';
+            this.showNextTaskFollowup = false;
+            this.selectedTaskIndex = null;
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    .CobaiaApp {
-        font-size: 0.75rem;
-        font-style: italic;
+    .a-RandomTask {
+        ol {
+            padding-left: 0;
+        }
+
+        li {
+            list-style-type: none;
+        }
+
+        li.is-done span:first-child {
+            color: lightgray;
+            font-style: italic;
+            text-decoration: line-through;
+        }
     }
 </style>
 
