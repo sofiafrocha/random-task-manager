17eff2fe637a8f01f7fd7ad59d0fc211f66f168b
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2020-02-11 22:44:32 +0000

    Add function that prompts the browser to download a given JSON

---
+++ assets/js/export_tasks.js
@@ -0,0 +1,10 @@
+// From https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+function downloadObjectAsJson(exportObj, exportName) {
+	const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
+	const downloadAnchorNode = document.createElement('a');
+	downloadAnchorNode.setAttribute('href', dataStr);
+	downloadAnchorNode.setAttribute('download', `${exportName}.json`);
+	document.body.appendChild(downloadAnchorNode); // required for firefox
+	downloadAnchorNode.click();
+	downloadAnchorNode.remove();
+}
