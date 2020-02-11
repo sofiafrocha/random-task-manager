517b26837598bbfe526656c9e0fd2262f602c716
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2020-02-11 22:54:19 +0000

    Split into smaller functions

--- assets/js/export_tasks.js
+++ assets/js/export_tasks.js
@@ -1,10 +1,23 @@
-// From https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
-function downloadObjectAsJson(exportObj, exportName) {
-	const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
-	const downloadAnchorNode = document.createElement('a');
-	downloadAnchorNode.setAttribute('href', dataStr);
-	downloadAnchorNode.setAttribute('download', `${exportName}.json`);
-	document.body.appendChild(downloadAnchorNode); // required for firefox
-	downloadAnchorNode.click();
-	downloadAnchorNode.remove();
+// Based off https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+
+function buildDataString(jsonObject) {
+	return `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(jsonObject))}`;
+}
+
+function addDataToElement(data, element, fileName = 'export') {
+	element.setAttribute('href', data);
+	element.setAttribute('download', `${fileName}.json`);
+	document.body.appendChild(element); // required for firefox
+}
+
+function triggerDownloadWindow(element) {
+	element.click();
+	element.remove();
+}
+
+export default function promptUserToDownloadJSON(jsonObject) {
+	const data = buildDataString(jsonObject);
+	const temporaryNode = document.createElement('a');
+	addDataToElement(data, temporaryNode);
+	triggerDownloadWindow(temporaryNode);
 }
